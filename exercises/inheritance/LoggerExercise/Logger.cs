9113c93457005c6bb84b1dea9764a1e0bd4c2267
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2023-03-22 12:46:25 +0100

    Logger Exercise

---
+++ exercises/inheritance/LoggerExercise/Logger.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace LoggerExercise
+{
+    public abstract class Logger
+    {
+
+        public abstract void Log(String message);
+
+
+        public virtual void Close() { }
+    }
+
+    public class StreamLogger : Logger
+    {
+        StreamWriter writer = null;
+        public StreamLogger(StreamWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public override void Log(String message)
+        {
+            writer.WriteLine(message);
+            writer.Flush();
+        }
+
+    }
+
+    public class FileLogger : StreamLogger
+    {
+        FileStream stream = null;
+
+        public static FileLogger Create(string filename)
+        {
+            FileStream stream = File.OpenWrite(filename);
+            return new FileLogger(stream);
+        }
+
+        public FileLogger(FileStream stream)
+            : base(new StreamWriter(stream))
+        {
+            this.stream = stream;
+
+        }
+
+        public override void Close()
+        {
+            stream.Close();
+        }
+    }
+
+    public class NullLogger : Logger
+    {
+        public override void Log(String message){
+
+        }
+    }
+
+    public class LogBroadcaster : Logger
+    {
+        IEnumerable<Logger> loggers = null;
+        public LogBroadcaster(IEnumerable<Logger> loggers)
+        {
+            this.loggers = loggers;
+        }
+        public override void Log(String message)
+        {
+            foreach (var log in loggers)
+            {
+                log.Log(message);
+            }
+        }
+    }
+}
+
