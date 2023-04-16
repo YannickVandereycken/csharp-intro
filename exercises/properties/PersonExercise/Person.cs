1bf066dc0e30c7371f9499d19e68447695b49f00
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2023-04-16 18:53:26 +0200

    properties ex

---
+++ exercises/properties/PersonExercise/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace PersonExercise
+{
+	public class Person
+	{
+        public Person(String name, int age)
+		{
+			this.Name = name;
+			this.Age = age;
+		}
+
+        private string name;
+
+        private int age;
+
+        public String Name
+        {
+			get {
+				return this.name;
+			}
+			set {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentException("Cant be null or empty");
+				this.name = value;
+					}
+		}
+
+		public int Age
+        {
+			get
+			{
+				return this.age;
+			}
+			set
+			{
+                if (value <0) throw new ArgumentException("Cant be negative");
+                this.age = value;
+			}
+		}
+	}
+}
+
---
+++ exercises/properties/TimeExercise/Time.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace TimeExercise
+{
+	public class Time
+	{
+		public Time(int hours, int minutes, int seconds)
+		{
+			this.Hours = hours;
+			this.Minutes = minutes;
+			this.Seconds = seconds;
+		}
+
+		private int seconds;
+		private int minutes;
+		private int hours;
+
+		public int Seconds
+		{
+			get
+			{
+				return this.seconds;
+			}
+			set
+			{
+				if (value < 0 || value > 59) throw new ArgumentException("Must be between 0-59");
+				this.seconds = value;
+			}
+		}
+        public int Minutes
+        {
+            get
+            {
+                return this.minutes;
+            }
+            set
+            {
+                if (value < 0 || value > 59) throw new ArgumentException("Must be between 0-59");
+                this.minutes = value;
+            }
+        }
+
+		public int Hours { get; set; }
+
+		public int TotalSeconds => (int)((Hours * 60 * 60) + (Minutes * 60) + Seconds);
+    }
+}
+
